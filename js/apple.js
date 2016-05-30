ecdec3ad990fd65b3a86b813c0805299e2d0f962
coproductions <constantin.dumba@gmail.com>
2016-05-30 12:06:20 -0700

    adds bounce to apple class

--- js/apple.js
+++ js/apple.js
@@ -31,10 +31,29 @@ My.Apple.prototype.move = function(){
   this.x += this.vector.vx;    // adjust vector position
   this.y += this.vector.vy;
 
-  //when object hits the ground, mask as landed
+  //when object hits the ground, bounce
   if (this.y > this.ctx.canvas.height *0.9){
-    this.landed = true;
+    this.bounce();
   }
   return true;
+};
+//if  bounce function is called continously until apple comes to rest
+My.Apple.prototype.bounce = function(){
+  // base case, if apple is rebounding ignore
+  if(this.vector.vy < 0){
+    return;
+  //bascase if downward ball has slowed down sufficiently, mark as landed
+  } else if(this.vector.vy < 1){
+    this.vector.vy = 0;
+    if(this.vector.vx < 0.1){
+      this.landed = true;
+      return;
+    }
+  }
+  // otherwise reverse the vertical movement, decrease horizontal, and decrease gravity and continue moving
+  this.gravity *= 0.7;
+  this.vector.vy *= -0.5;
+  this.vector.vx *= 0.5;
+
 };
 
